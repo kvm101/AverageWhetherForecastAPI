ba68c80f8875c97876bbefe83f190996489dc29d
zeroDouBobaFett <vasylko2005@gmail.com>
2025-01-21 21:44:12 +0200

    replace variable client instead of function getRedisc()

--- awf/api.go
+++ awf/api.go
@@ -2,11 +2,18 @@ package awf
 
 import (
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
+
+var red = Redis{redis.NewClient(&redis.Options{
+	Addr:     "127.0.0.1:9736",
+	Password: "",
+	DB:       0,
+})}
 
 func HandlerWCurrent(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		getValue("")
 
 		w.WriteHeader(http.StatusOK)
 	}
--- awf/redis.go
+++ awf/redis.go
@@ -8,37 +8,30 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-func getRedisC() *redis.Client {
-	client := redis.NewClient(&redis.Options{
-		Addr:     "127.0.0.1:9736",
-		Password: "",
-		DB:       0,
-	})
+//	client := redis.NewClient(&redis.Options{
+//		Addr:     "127.0.0.1:9736",
+//		Password: "",
+//		DB:       0,
+//	})
+//
+// ping, err := client.Ping(context.Background()).Result()
 
-	ping, err := client.Ping(context.Background()).Result()
-	if err != nil {
-		log.Println(err)
-		return nil
-	}
-
-	log.Println(ping)
-	return client
+type Redis struct {
+	client *redis.Client
 }
 
-func setValue(key string, value interface{}, expiration time.Duration) {
-	client := getRedisC()
-	err := client.Set(context.Background(), key, value, expiration).Err()
+func (r *Redis) setValue(key string, value interface{}, expiration time.Duration) {
+	err := r.client.Set(context.Background(), key, value, expiration).Err()
 	if err != nil {
 		log.Println(err)
 		return
 	}
 }
 
-func getValue(key string) interface{} {
+func (r *Redis) getValue(key string) interface{} {
 	var data interface{}
 
-	client := getRedisC()
-	err := client.Get(context.Background(), key).Scan(&data)
+	err := r.client.Get(context.Background(), key).Scan(&data)
 	if err != nil {
 		log.Println(err)
 		return nil
